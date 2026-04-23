6fbf41dba3343e037c4a5572ae0ef68cc00db4b2
Manulithic <manojganesh615@gmail.com>
2026-04-23 16:28:36 +0530

    disk usage monitor

---
+++ disk-usage.py
@@ -0,0 +1,12 @@
+import subprocess
+
+result = subprocess.run(["df", "-h"], capture_output=True, text=True)
+
+for line in result.stdout.split("\n"):
+  parts = line.split()
+  if len(parts) > 5 and parts[-6] == "/dev/root":
+    usage = int(part[-2].replace("%", ""))
+    if usage > 70:
+      print ("High Disk Usage", usage, %)
+    else:
+      print("Normal Disk Usage", usage, %)
