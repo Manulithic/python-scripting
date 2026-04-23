9a2a1d76b38b3507cff3bc37b773cc41c64cab5f
Manulithic <manojganesh615@gmail.com>
2026-04-23 16:31:40 +0530

    Update disk-usage.py

--- disk-usage.py
+++ disk-usage.py
@@ -5,7 +5,7 @@ result = subprocess.run(["df", "-h"], capture_output=True, text=True)
 for line in result.stdout.split("\n"):
   parts = line.split()
   if len(parts) > 5 and parts[-6] == "/dev/root":
-    usage = int(part[-2].replace("%", ""))
+    usage = int(parts[-2].replace("%", ""))
     if usage > 70:
       print ("High Disk Usage", usage, "%")
     else:
