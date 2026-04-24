2b7491129d885eb25ca12426b0b61314fe5d60ef
Manoj G <manojganesh615@gmail.com>
2026-04-24 17:06:53 +0530

    Add disk usage alert script
    This script checks disk usage and alerts if usage exceeds 70%.

---
+++ beginner/disk-usage-alert.py
@@ -0,0 +1,12 @@
+import subprocess
+#subprocess is a python module that allows python to run shell commands and interact with OS
+result = subprocess.run(["df", "-h"], capture_output=True, text=True)
+
+for line in result.stdout.split("\n"):
+  parts = line.split()
+  if len(parts) > 5 and parts[-6] == "/dev/root":
+    usage = int(parts[-2].replace("%", ""))
+    if usage > 70:
+      print ("High Disk Usage", usage, "%")
+    else:
+      print("Normal Disk Usage", usage, "%")
