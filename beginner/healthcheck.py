a86db5a4e687f906aa37521e56b2fdb99e28e931
Manoj G <manojganesh615@gmail.com>
2026-04-28 13:01:55 +0530

    Enhance healthcheck.py to support multiple URLs
    Refactor health check script to handle multiple URLs and report response time.

--- beginner/healthcheck.py
+++ beginner/healthcheck.py
@@ -2,19 +2,24 @@
 import requests
 import sys
 
-url = sys.argv[1]   #if you want to pass the url as command line arguments
+urls = sys.argv[1:]   #if you want to pass the url as command line arguments [ e.g. python3 healthcheck.py https://www.google.com https://www.facebook.com ]
+#url = sys.argv[1] if you're just passing one url for health check
 #url = "https://www.google.com" if you want to hard code the url
+#urls = ["https://www.google.com", "https://www.facebook.com"] if you want to hard code multiple urls
 
-response = requests.get(url, timeout=5)
-result = response.status_code
+exit_code = 0
 
-try:
-  if result == 200:
-    print("Healt check is passed:", result)
-    sys.exit(0) #success
-  else:
-    print("Health check is failed:", result)
-    sys.exit(2)  #warning
-except requests.exceptions.RequestException:
+for url in urls:
+  try:
+    response = requests.get(url, timeout=5)
+    result = response.status_code
+    response_time = requests.get(url, timeout=5).elapsed.total_seconds()
+    if result == 200:
+      print(url, "is up and status code is", result, "and response time is:", response_time)
+    else:
+      print(url, "is down and status code is", result, "and response time is:", response_time)
+      exit_code = 1
+  except requests.exceptions.RequestException:
     print("Service Unreachable")
-    sys.exit(1) #failure
+
+sys.exit(exit_code)
