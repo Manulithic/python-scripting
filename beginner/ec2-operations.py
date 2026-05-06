5fc7ab250a0cf391ba405f13f0bd5a82958776ca
Manoj G <manojganesh615@gmail.com>
2026-05-06 22:28:08 +0530

    start EC2 instances by name
    Added a script to start multiple EC2 instances by their names using Boto3.

--- beginner/ec2-operations.py
+++ beginner/ec2-operations.py
@@ -47,4 +47,29 @@ print(stopped_instances)
 print("Below are the EC2 instances in terminated state:")
 print(terminated_instances)
 
+---------------------------------------------------------------------------------------------------------------------------------
+######SCRIPT-3#######
+######To Start Multiple Instances by it's names#######
+
+#!/usr/bin/python3
+
+import boto3
+import sys
+
+names = sys.argv[1:]
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_instances()
+
+for name in names:
+  for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+      instance_id = instance['InstanceId']
+      for tag in instance['Tags']:
+        if tag['Key'] == 'Name' and tag['Value'] == name:
+          ec2.start_instances(InstancesIds=[instance_id])
+
+#USAGE: python3 start-instances.py <instance-name-1> <instance-name-2>......
+
+------------------------------------------------------------------------------------------------------------------------------------
 
