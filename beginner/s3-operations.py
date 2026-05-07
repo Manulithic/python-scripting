dd74522a79cede103eabf7a2cf6d190640c2f80a
Manoj G <manojganesh615@gmail.com>
2026-05-07 16:24:49 +0530

    Implement script to fetch bucket details by name
    Added a script to list bucket ARN and creation date based on bucket name input.

--- beginner/s3-operations.py
+++ beginner/s3-operations.py
@@ -14,3 +14,26 @@ for bucket in response['Buckets']:
   print(bucket_name, bucket_arn)
 
 ----------------------------------------------------------------------------------
+######SCRIPT-2########
+######LIST BUCKET ARN & CREATION DATE WITH BUCKET NAME AS INPUT#######
+
+#!/usr/bin/python3
+
+import boto3
+import sys
+
+names = sys.argv[1:]
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+if len(names) < 1:
+  print("ERROR: Please Enter Bucket Name to fetch Details.")
+  print("USAGE: python3 fetch-bucket-details.py <BUCKET-NAME>")
+  
+for name in names:
+  for bucket in response['Buckets']:
+    bucket_name = bucket['Name']
+    bucket_arn = bucket['BucketArn']
+    bucket_creation_date = bucket['CreationDate']
+    if bucket_name == name:
+      print(bucket_name, bucket_arn, bucket_creation_date)
