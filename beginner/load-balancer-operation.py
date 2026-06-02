6d3688729bd7e007b16bc2cfb80e2032bd5d8b50
Manoj G <manojganesh615@gmail.com>
2026-06-02 13:19:06 +0530

    load balancer details listing script
    Added a script to list details of load balancers by name from input.

--- beginner/load-balancer-operation.py
+++ beginner/load-balancer-operation.py
@@ -29,3 +29,38 @@ for balancer in public_load_balancers:
 logger.info("Private Load Balancers")
 for balancer in private_load_balancers:
     print(balancer['LoadBalancerName'], balancer['LoadBalancerArn'])
+
+-------------------------------------------------------------------------------------------------------------------------
+#################SCRIPT-2#######################
+##############LIST DETAILS OF LB BY IT'S NAME AS INPUT##################
+
+#!/usr/bin/python3
+
+import sys
+import boto3
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+lbs = sys.argv[1:]
+elbv2 = boto3.client('elbv2')
+
+response = elbv2.describe_load_balancers()
+
+logger.info("fetching load balancer details")
+
+for lb in lbs:
+    found = False
+    for balancer in response['LoadBalancers']:
+        arn = balancer['LoadBalancerArn']
+        scheme = balancer['Scheme']
+        lbtype = balancer['Type']
+        if balancer['LoadBalancerName'] == lb:
+            found = True
+            print(lb, arn, scheme, lbtype)
+    if not found:
+        logger.info(f"load balancer is not found")
+
+-------------------------------------------------------------------------------------------------------------------------
